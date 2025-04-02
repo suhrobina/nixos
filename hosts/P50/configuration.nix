87d3dfe1bfaab664655a783761df721ebf6da5b6
Suhrob R. Nuraliev <LongOverdueVitalEnergy@GMail.com>
2025-04-02 22:11:03 +0500

    Commit on changes

--- hosts/P50/configuration.nix
+++ hosts/P50/configuration.nix
@@ -227,8 +227,10 @@
     davinci-resolve
     gparted
     rustdesk
-    gnupg gpg-tui
+    gnupg gpg-tui kleopatra
     appimage-run
+    steam-run
+    pidgin
    ];
 
 
@@ -266,11 +268,12 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    #pinentryPackage = pkgs.pinentry;
+    #enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
--- modules/openbox.nix
+++ modules/openbox.nix
@@ -4,10 +4,17 @@
   # Enable OpenBox WM
   services.xserver.windowManager.openbox.enable = true;
 
+  # Configure lid begavior
+#  services.logind = {
+#    lidSwitch = "ignore";              # Do nothing when lid is closed (on battery or AC)
+#    lidSwitchDocked = "ignore";        # Do nothing when docked
+#    lidSwitchExternalPower = "ignore"; # Do nothing when on external power
+#  };
+
   environment.systemPackages = with pkgs; [
-    openbox-menu
     obconf
     tint2
     nitrogen
+    xlockmore
   ];
 }
