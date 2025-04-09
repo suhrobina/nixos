24394f65554b44c50c397afa2bacdc8f5a47696b
Suhrob R. Nuraliev <LongOverdueVitalEnergy@GMail.com>
2025-04-09 23:39:59 +0500

    Commit on changes

--- modules/openbox.nix
+++ modules/openbox.nix
@@ -5,11 +5,11 @@
   services.xserver.windowManager.openbox.enable = true;
 
   # Configure lid begavior
-  services.logind = {
-    lidSwitch = "ignore";              # Do nothing when lid is closed (on battery or AC)
-    lidSwitchDocked = "ignore";        # Do nothing when docked
-    lidSwitchExternalPower = "ignore"; # Do nothing when on external power
-  };
+  #services.logind = {
+  #  lidSwitch = "ignore";              # Do nothing when lid is closed (on battery or AC)
+  #  lidSwitchDocked = "ignore";        # Do nothing when docked
+  #  lidSwitchExternalPower = "ignore"; # Do nothing when on external power
+  #};
 
   environment.systemPackages = with pkgs; [
     obconf
